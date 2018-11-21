0dd47308ba9f3ed756881c412a04b515f4b31e11
Elias Ferguson <eferguson00@stuy.edu>
2018-11-21 11:32:08 -0500

    beginning work on the constructor

---
+++ MyString.java
@@ -0,0 +1,9 @@
+public class MyString implements CharSequence,Comparable<CharSequence>{
+  private char[] data;
+  public MyString(CharSequence s){
+    data = new char[s.length];
+    for (int i = 0 ; i < s.length; i++) {
+      data[i] = s.charAt(i);
+    }
+  }
+}
