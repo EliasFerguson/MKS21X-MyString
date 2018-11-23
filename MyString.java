5b163add07f5d8e904aba89cb9ad197b4c8b374d
Elias Ferguson <eferguson00@stuy.edu>
2018-11-23 14:45:09 -0500

    added haeders

--- MyString.java
+++ MyString.java
@@ -1,9 +1,21 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
-  public MyString(CharSequence s){
-    data = new char[s.length];
-    for (int i = 0 ; i < s.length; i++) {
+  public MyString(CharSequence s) {
+    data = new char[s.length()];
+    for (int i = 0 ; i < s.length(); i++) {
       data[i] = s.charAt(i);
     }
+  }
+  public char charAt(int idx) {
+
+  }
+  public int length() {
+
+  }
+  public CharSequence subSequence(int start, int end) {
+
+  }
+  public String toString() {
+
   }
 }
