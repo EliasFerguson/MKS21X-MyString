0793cc6fc9b123c2f931c3699314c0f796a0c7df
Elias Ferguson <eferguson00@stuy.edu>
2018-11-23 14:54:31 -0500

    added prelim charAt

--- MyString.java
+++ MyString.java
@@ -1,5 +1,6 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
+  String indexProblem = "Index must be greater than 0 and less than the length of the CharSequence.";
   public MyString(CharSequence s) {
     data = new char[s.length()];
     for (int i = 0 ; i < s.length(); i++) {
@@ -7,7 +8,12 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
     }
   }
   public char charAt(int idx) {
-
+    if (idx < 0 || idx > (data.length - 1)) {
+      throw new IndexOutOfBoundsException(indexProblem);
+    }
+    else {
+      return data[idx];
+    }
   }
   public int length() {
 
